4fcad61f6479280c86d8be88ef8e8bd5331826f4
tuanphong511 <tuanphong511@gmail.com>
2023-09-13 09:13:30 +0700

    done cập nhật thông tin cá nhân

--- src/pages/proFile/FormEditProfile.js
+++ src/pages/proFile/FormEditProfile.js
@@ -1,73 +1,99 @@
 import { Button, TextField, Typography } from "@mui/material";
 import * as React from "react";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import customAxios from "../../services/api";
 import Box from "@mui/material/Box";
-import {useNavigate} from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 
 export default function FormEditProfile() {
-    const navigate = useNavigate()
-    const [profile ,setProfile]= React.useState({})
-    const user = JSON.parse(localStorage.getItem("user"))
+    const navigate = useNavigate();
+    const [profile, setProfile] = useState({
+        username : "",
+        telephone : ""
+    });
+    const user = JSON.parse(localStorage.getItem("user"));
 
     useEffect(() => {
         if (!user || !user.message || !user.message.token || !user.message.token.idUser) {
-            navigate('/');
+            navigate("/");
             return;
         }
 
         const id = user.message.token.idUser;
 
-        customAxios(`/users/?id=${id}`).then((res) => {
-            setProfile(res.data);
-            // Lấy dữ liệu người dùng (chẳng hạn, thông tin về mật khẩu hiện tại) từ API
-        }).catch((error) => {
-            console.error(error);
-            // Handle error when unable to fetch user data
-        });
-    }, [user, navigate]);
+        customAxios(`/users/?id=${id}`)
+            .then((res) => {
+                setProfile(res.data);
+                // Lấy dữ liệu người dùng (chẳng hạn, thông tin về mật khẩu hiện tại) từ API
+            })
+            .catch((error) => {
+                console.error(error);
+                // Xử lý khi không thể lấy dữ liệu người dùng
+            });
+    }, []);
 
+
+
+    const handleSubmit = () => {
+        const profiles = {
+            username : profile.username,
+            telephone : profile.telephone,
+            password : profile.password,
+            role: profile.role,
+            avatar: profile.avatar,
+            status:profile.status
+
+        };
+        console.log("update",profiles)
+        customAxios
+            .put(`/users/${profile.id}`, profiles)
+            .then(() => {
+                alert("Đã sửa thành công");
+                navigate("/");
+            })
+            .catch((error) => {
+                console.error(error);
+                // Xử lý khi không thể cập nhật thông tin người dùng
+            });
+    };
+    console.log(profile)
     return (
         <div>
             <Box
                 initialValues={profile}
                 enableReinitialize={true}
-                onSubmit={(values) =>{
-                    customAxios.put(`/users/${profile.id}` ,values).then(()=>{
-                        alert("da sua thanh cong")
-                        navigate("/")
-                    })
-                }}
+                onSubmit={handleSubmit}
             >
-            <Typography variant="h4" component="h2" sx={{ mb: 2 }}>
-                Chỉnh sửa thông tin
-            </Typography>
-            <TextField
-                id={"username"}
-                sx={{ mb: 2, width: "100%" }}
-                name={"username"}
-                value={profile.username}
-                onChange={(e) =>{
-                    setProfile({ ...profile, username: e.target.value })
-                }}
-            />
+                <Typography variant="h4" component="h2" sx={{ mb: 2 }}>
+                    Chỉnh sửa thông tin
+                </Typography>
+                <TextField
+                    id="username"
+                    sx={{ mb: 2, width: "100%" }}
+                    name="username"
+                    value={profile.username || ""}
+                    onChange={(e) =>
+                        setProfile({...profile, username: e.target.value})
+                    }
+                />
 
-            {/*<TextField*/}
-            {/*    label="Address"*/}
-            {/*    sx={{ mb: 2, width: "100%" }}*/}
-            {/*/>*/}
-            <TextField
-                id={"telephone"}
-                sx={{ mb: 2, width: "100%" }}
-                name={"telephone"}
-                value={profile.telephone}
-                onChange={(e) =>{
-                    setProfile({ ...profile, telephone: e.target.value })
-                }}
-            />
-            <Button variant="contained" sx={{width: "100%"}}>Xác nhận</Button>
+                {/*<TextField*/}
+                {/*    label="Address"*/}
+                {/*    sx={{ mb: 2, width: "100%" }}*/}
+                {/*/>*/}
+                <TextField
+                    id="telephone"
+                    sx={{ mb: 2, width: "100%" }}
+                    name="telephone"
+                    value={profile.telephone}
+                    onChange={(e) =>
+                        setProfile({...profile, telephone: e.target.value})
+                    }
+                />
+                <Button variant="contained" sx={{ width: "100%" }} onClick={handleSubmit}>
+                    Xác nhận
+                </Button>
             </Box>
         </div>
-    )
+    );
 }
